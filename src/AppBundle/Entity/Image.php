a40898a7c3c5ea2ce1157709711efd608f86b0e6
RAISON Yoann <yoann.raison@uca.fr>
2019-10-14 18:10:49 +0200

    add faction folder to upload tactical cards && add image entity

---
+++ src/AppBundle/Entity/Image.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace AppBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Constraints;
+
+/**
+ * Image
+ *
+ * @ORM\Table(name="image")
+ * @ORM\Entity(repositoryClass="AppBundle\Repository\ImageRepository")
+ */
+class Image
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     * @ORM\Column(name="image", type="string", length=255, unique=true)
+     * @Constraints\NotBlank(message="Please, upload the product brochure as a PDF file.")
+     * @Constraints\File(mimeTypes={ "application/pdf" })
+     */
+    private $image;
+
+    /**
+     * Get id.
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @return string
+     */
+    public function getImage()
+    {
+        return $this->image;
+    }
+
+    /**
+     * @param string $image
+     */
+    public function setImage($image)
+    {
+        $this->image = $image;
+    }
+
+}

9bc35369104cc4548b55989154d5df649a2761b7
RAISON Yoann <yoann.raison@uca.fr>
2019-11-14 20:14:45 +0100

    fix bug order disabled ?

--- src/AppBundle/Resources/public/js/script.js
+++ src/AppBundle/Resources/public/js/script.js
@@ -429,8 +429,9 @@ function getIndividus(factionId, typeId, selectId, modalId, btnToAdd, idUCrattac
             // if((msg[individuInfo].type == 1  && msg[individuInfo].isUnique &&  ($('*[data-id="'+msg[individuInfo].id+'"]').length == 1 )) || (msg[individuInfo].libelleSpecial == 'processByTwo' && $('*[data-id="'+msg[individuInfo].id+'"]').length < 2 ))
             if((msg[individuInfo].type == 1  && msg[individuInfo].isUnique &&  ($('*[data-id="'+msg[individuInfo].id+'"]').length == 1 )) || (msg[individuInfo].libelleSpecial == 'processByTwo' ))
                 ul += buildLi(msg, individuInfo, selectId, typeId, idAttchBtnToReplace, btnToAdd, idUCrattach);
-            else if(!msg[individuInfo].isUnique)
+            else if(msg[individuInfo].isUnique &&  $('*[data-id="'+msg[individuInfo].id+'"]').length == 0) {
                 ul += buildLi(msg, individuInfo, selectId, typeId, idAttchBtnToReplace, btnToAdd, idUCrattach);
+            }
             else if(msg[individuInfo].isUnique &&  ($('*[data-id="'+msg[individuInfo].id+'"]').length != 0 ))
                 ulDisabled += buildLi(msg, individuInfo, selectId, typeId, idAttchBtnToReplace, btnToAdd);
         }
